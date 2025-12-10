06dbf1713e951ef4761047a559c8e7fb7fbd3af1
ELLIGEE <joaoguilherme.david@hotmail.com>
2025-12-09 23:49:24 -0300

    update adicionado a navbar

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -7,20 +7,20 @@ import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001
-
-connectDB();
+const PORT = process.env.PORT || 5001;
 
 //middleware
-app.use(express.json())
-app.use(rateLimiter)
+app.use(express.json());
+app.use(rateLimiter);
 //app.use((req, res, next) => {
 //  console.log(`Metodo req é ${req.method} & a URL da req é ${req.url}`)
 //  next();
 //})
 
-app.use("/api/notes", noteRoutes);
+connectDB().then(() => {
+  app.use("/api/notes", noteRoutes);
 
-app.listen(PORT, () => {
-  console.log("server esta rodando na PORT:", PORT);
+  app.listen(PORT, () => {
+    console.log("server esta rodando na PORT:", PORT);
+  });
 });
